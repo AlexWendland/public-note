f32164e0e5cc44b7eb2b5faff9bcb2ca287560e7
Alex Wendland <a.p.wendland@gmail.com>
2023-10-08 20:32:37 +0100

    Sanity changes

--- scripts/obsidian_helper/update_last_edited.py
+++ scripts/obsidian_helper/update_last_edited.py
@@ -1,9 +1,9 @@
-import datetime
 import os
 from pathlib import Path
 
 from constants import OBSIDIAN_DIR
 from read_obsidian import read_obsidian_file
+from write_obsidian import write_obsidian_file
 
 directory = Path(OBSIDIAN_DIR)
 
@@ -11,6 +11,7 @@ for file in directory.rglob('*'):
     if file.is_file() and file.suffix == '.md':
 
         timestamp = os.path.getmtime(file)
-        reported_last_edited = read_obsidian_file(str(file)).metadata.get('last_edited', None)
+        # reported_last_edited = read_obsidian_file(str(file)).metadata.get('last_edited', None)
+        write_obsidian_file(read_obsidian_file(str(file)))
 
-        print(file, datetime.date.fromtimestamp(timestamp), reported_last_edited)
+        # print(file, datetime.date.fromtimestamp(timestamp), reported_last_edited)
