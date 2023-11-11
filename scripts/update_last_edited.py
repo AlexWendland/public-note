6ebd9665316a0afe827a87b2b1304243d4540648
Alex Wendland <a.p.wendland@gmail.com>
2023-11-11 13:51:07 +0000

    Update scripts to catch all files

--- scripts/update_last_edited.py
+++ scripts/update_last_edited.py
@@ -1,10 +1,10 @@
-import os
+# import os
 
 from obsidian_helper.read_obsidian import get_obsidian_files, read_obsidian_file
 from obsidian_helper.write_obsidian import write_obsidian_file
 
 for file in get_obsidian_files():
-    timestamp = os.path.getmtime(file)
+    # timestamp = os.path.getmtime(file)
     # reported_last_edited = read_obsidian_file(str(file)).metadata.get('last_edited', None)
     write_obsidian_file(read_obsidian_file(str(file)))
 
