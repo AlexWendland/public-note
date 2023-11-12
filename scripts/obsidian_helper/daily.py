470d2ab78accccf12b64cfbc574e981ad78fd26f
Alex Wendland <a.p.wendland@gmail.com>
2023-11-12 08:22:10 +0000

    vault backup: 2023-11-12 08:22:10

--- scripts/obsidian_helper/daily.py
+++ scripts/obsidian_helper/daily.py
@@ -1,6 +1,6 @@
 import logging
 
-from obsidian_helper import file_admin, index_admin, read_obsidian, write_obsidian
+from obsidian_helper import file_admin, index_admin, openai_note_check, read_obsidian, write_obsidian
 
 logger = logging.getLogger(__name__)
 
@@ -11,8 +11,8 @@ def main():
         logger.info(f"{file_path} INFO: Updating...")
         obsidian_file = read_obsidian.read_obsidian_file(str(file_path))
         file_admin.update_tags(obsidian_file)
-        # if not obsidian_file.metadata.get("checked", False):
-        #     openai_note_check.update_article(obsidian_file)
+        if not obsidian_file.metadata.get("checked", False):
+            openai_note_check.update_article(obsidian_file)
         logger.info(f"{file_path} INFO: Updated.")
         write_obsidian.write_obsidian_file(obsidian_file)
 
