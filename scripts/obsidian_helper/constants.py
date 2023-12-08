78d5a0a156d6caa237cbb43724749a37ce7c6fbd
Alex Wendland <a.p.wendland@gmail.com>
2023-12-08 15:30:27 +0000

    vault backup: 2023-12-08 15:30:26

--- scripts/obsidian_helper/constants.py
+++ scripts/obsidian_helper/constants.py
@@ -4,3 +4,4 @@ OBSIDIAN_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(_
 CHECKED_FIELD = "checked"
 LAST_EDITED_FIELD = "last_edited"
 INDEX_FILES = ["Git Index", "Maths Index", "Programming Index", "Python Index"]
+ALIAS_FIELD = "aliases"
